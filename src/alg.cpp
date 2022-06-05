c843f8f071ca5d72b76427a34e4634bfc91e7272
Anna Shevelyova <99125817+shvlvnn@users.noreply.github.com>
2022-06-05 19:27:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,10 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::string optionsTree = tree[n - 1];
+  std::vector<char> nums;
+  for (int x = 0; x < optionsTree.length(); x++) {
+    nums.push_back(optionsTree[x]);
+  }
+  return nums;
 }
